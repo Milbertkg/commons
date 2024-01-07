d662f714151e460b939aec665fda89bdf10f8185
Milbert <milbert.retana@kuantumk.com>
2024-01-07 16:13:20 -0600

    actualizacion para incluir la opcion dev al leer parametros

--- text/loadconfiguration/loadconfigurations.go
+++ text/loadconfiguration/loadconfigurations.go
@@ -8,11 +8,7 @@ import (
 	//"log"
 )
 
-func LoadConfigurations(conf_file string, param string) string {
-
-	var  isDev string
-
-	isDev = "0"
+func LoadConfigurations(conf_file string, param string, isDev string) string {
 
 	if isDev != "1" {
 
